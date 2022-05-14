aace488a2b744973f4f2f62f9aad57a5fc550f94
tom-gordon13 <tombcgordon@gmail.com>
2022-05-14 07:23:57 -0600

    Create superuser, verify navbar is dynamic

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('accounts/signup/', views.signup, name='signup')
+    path('accounts/signup/', views.signup, name='signup'),
+    path('test/', views.test, name='test')
 ,]
