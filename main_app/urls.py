56ec20954fcf140b8409c58e093f3e1296d4071c
Justin Hedrick <Justinchedrick@gmail.com>
2022-05-16 13:21:59 -0700

    added card to stock detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
     path('stocks/', views.stock_index, name='stock'),
+    path('stocks/<int:stock_id>/', views.stock_detail, name='stock_detail'),
 ]
