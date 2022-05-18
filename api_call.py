08282f8fd53f6b65295353331ad7c0e827f0a5c6
tom-gordon13 <tombcgordon@gmail.com>
2022-05-18 15:56:02 -0600

    Update API call to grab yesterday closing prices

--- api_call.py
+++ api_call.py
@@ -1,5 +1,6 @@
 run = True
-
+from datetime import date
+from datetime import timedelta
 
 def timedFunc():
     import requests
@@ -7,8 +8,9 @@ def timedFunc():
     import schedule, time
     from main_app.models import Stock
 
-
-    stock_data_raw_obj = requests.get(f'https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2022-05-13?adjusted=true&apiKey=ISRFyZyx4zGrz0Pzy3veu6ou4pPUYQjU').json()['results']
+    today = date.today()
+    yesterday = today - timedelta(days = 1)
+    stock_data_raw_obj = requests.get(f'https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/{yesterday}?adjusted=true&apiKey=ISRFyZyx4zGrz0Pzy3veu6ou4pPUYQjU').json()['results']
 
     for tckr in stock_data_raw_obj:
         currStock = Stock.objects.filter(ticker=(tckr['T']))
