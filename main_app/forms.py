e039a7cde5f640dee60409017101c4484d305b5e
tom-gordon13 <tombcgordon@gmail.com>
2022-05-18 12:49:13 -0600

    Fixing up form to display only user buckets

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,22 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ModelChoiceField
+from django import forms
+from django.contrib.auth.models import User
 from .models import Stock, Bucket, StockInstance
 
+
+
+
 class StockForm(ModelForm):
+  
   class Meta:
     model = StockInstance
     fields = ['stock', 'bucket']
+
+  def __init__(self, *args, **kwargs):
+          
+          super(StockForm, self).__init__(*args, **kwargs)
+          if self.instance:
+              self.fields['bucket'].queryset = Bucket.objects.filter(user = 4)
+
+
+      
